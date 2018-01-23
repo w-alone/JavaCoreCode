8b2b8bab53186efd7bf9e533fcaf520e5bbf4e78
w-alone <wangruilong0212@gmail.com>
2018-01-23 11:03:27 +0800

    -

---
+++ bounce/BallComponent.java
@@ -0,0 +1,27 @@
+package bounce;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+public class BallComponent extends JPanel{
+	private static final int DEFAULT_WIDTH = 450;
+	private static final int DEFAULT_HEIGHT = 450;
+	private java.util.List<Ball> balls = new ArrayList<>();
+	
+	public void add(Ball b) {
+		balls.add(b);		
+	}
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		for (Ball b : balls) {
+			g2.fill(b.getShape());
+		}
+	}
+
+	public Dimension getPreferredSize(){
+		return new Dimension(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+	}
+}
